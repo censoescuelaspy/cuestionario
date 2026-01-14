f31e2fe45a82ab6e2c2b12e78e5f2be2f6983293
censoescuelaspy <censoescuelaspy@gmail.com>
2026-01-14 05:20:52 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@
 export const CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwrWr3KuPsx6rH1ObV9eALIR-vaIC3Hma1qnhSG0MNQIeWoxov19b9YjRCJ0EjO8wFefw/exec",
   APP_NAME: "SIDIE - Relevamiento de Infraestructura",
-  APP_VERSION: "0.2.0",
+  APP_VERSION: "0.2.1",
   MAX_SCHOOL_SUGGESTIONS: 200,
   PHOTO_MAX_MB: 8
 };
