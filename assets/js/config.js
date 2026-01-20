2cc5d7d20e95271088a2bc8a078258499d0c0129
censoescuelaspy <censoescuelaspy@gmail.com>
2026-01-20 17:45:19 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,7 @@
 // Reemplace APPS_SCRIPT_URL por la URL de despliegue (Web App) de Google Apps Script.
 export const CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwrWr3KuPsx6rH1ObV9eALIR-vaIC3Hma1qnhSG0MNQIeWoxov19b9YjRCJ0EjO8wFefw/exec",
+  //https://script.google.com/macros/s/AKfycbwrWr3KuPsx6rH1ObV9eALIR-vaIC3Hma1qnhSG0MNQIeWoxov19b9YjRCJ0EjO8wFefw/exec
   APP_NAME: "SIDIE - Relevamiento de Infraestructura",
   APP_VERSION: "0.2.1",
   MAX_SCHOOL_SUGGESTIONS: 200,
